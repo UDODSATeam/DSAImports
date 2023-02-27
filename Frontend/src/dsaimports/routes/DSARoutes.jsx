ffbe846aafffc397f39a37fcdf4d05b92c4be094
unknown <gamboadavid1998@gmail.com>
2023-02-27 00:40:54 -0400

    adding manager component

--- Frontend/src/dsaimports/routes/DSARoutes.jsx
+++ Frontend/src/dsaimports/routes/DSARoutes.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
-
+import { Gerente } from './gerente/gerente.component'
 export const DSARoutes = () => {
   return (
     <Routes>
+        <Route path='/dsaimports/gerente' element={<Gerente/>} />
+
+
         {/* TODO: Colocar las rutas separadas para cada tipo de vista (Admin, Gerente, Cobrador, etc etc) */}
         {/* TODO: Simular la separación de las rutas de acuerdo al tipo de usuario. */}
 
